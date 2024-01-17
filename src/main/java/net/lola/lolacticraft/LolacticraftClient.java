3308dfac127a9d6dc0ad9be312f089c0f32ecca2
Lola <91732346+LolaThePretty@users.noreply.github.com>
2024-01-17 04:08:32 +0100

    Initial commit

---
+++ src/main/java/net/lola/lolacticraft/LolacticraftClient.java
@@ -0,0 +1,9 @@
+package net.lola.lolacticraft;
+import net.fabricmc.api.ClientModInitializer;
+public class LolacticraftClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+
+    }
+}
---
+++ src/main/java/net/lola/lolacticraft/LolacticraftDataGenerator.java
@@ -0,0 +1,11 @@
+package net.lola.lolacticraft;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class LolacticraftDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
